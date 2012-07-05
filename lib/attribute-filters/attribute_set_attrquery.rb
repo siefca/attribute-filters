f856d0b6f669cafc3ec5a1d84030e99870b5fcee
Paweł Wilk <siefca@gnu.org>
2012-07-05 15:19:18 +0200

    Added DSL keyword for querying which attribute sets are associated with an attribute

--- lib/attribute-filters/attribute_set_attrquery.rb
+++ lib/attribute-filters/attribute_set_attrquery.rb
@@ -19,24 +19,45 @@ module ActiveModel
       # intercepted. Is allows to create semi-natural
       # syntax when querying attribute sets containing set names.
       # 
+      # When the called method name ends with question mark then
+      # its name is considered to be an attribute set name that
+      # should be tested for presence of the attribute. To use
+      # that syntax you must be sure that there is no already
+      # defined method for AttributeSet object which name ends
+      # with question mark. Otherwise you may get false positives
+      # or a strange errors when trying to test if attribute belongs
+      # to set. The real method call will override your check.
+      # 
       # @example
       #   the_attribute(:some_attribute).is.in?(:some_set)
       #   the_attribute(:some_attribute).list.sets
+      #   the_attribute(:some_attribute).is.in.a.set.that?(:should_be_downcased)
+      #   the_attribute(:some_attribute).should_be_downcased?
       # 
       # @param method_sym [Symbol,String] name of method that will be queued or called on a set
       # @param args [Array] optional arguments to be passed to a method call
       # @yield optional block to be passed to a method call
       def method_missing(method_sym, *args, &block)
         case method_sym.to_sym
-        when :are, :is, :in, :list, :show, :be, :should, :sets, :in_sets
+        when :are, :is, :one, :is_one, :in, :list, :show, :be, :should,
+             :the, :a, :sets, :in_sets, :set, :in_a_set, :in_set, :belongs_to
           self
-        when :in?, :in_set?, :in_a_set?, :in_the_set?, :the_set?, :set?
-          args = args.map{|a|a.to_sym if a.respond_to?(:to_sym)} if args.is_a?(Array) && args.present?
+        when :belongs_to?, :in?, :in_set?, :in_a_set?, :in_the_set?,
+             :the_set?, :set?, :is_one_that?, :one_that?, :that?
+          if args.present? && args.is_a?(::Array)
+            args = args.map{ |a| a.to_sym if a.respond_to?(:to_sym) }
+          end
           @set_object.include?(*args, &block)
         else
-          @set_object.method(method_sym).call(*args, &block)
+          set_name_str = method_sym.to_s.dup
+          if !@set_object.respond_to?(method_sym) && set_name_str.slice!(/\?\z/) == '?'
+            @set_object.include?(set_name_str.to_sym, &block)
+          else
+            @set_object.method(method_sym).call(*args, &block)
+          end
         end
       end
+
     end # class AttrQuery
   end # class AttributeSet
 end # module ActiveModel
