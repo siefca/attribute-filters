40db8f483d43bd894bff7437e9582c82f6f6db6a
Paweł Wilk <siefca@gnu.org>
2012-07-08 10:11:34 +0200

    Added the Common module with common filtering methods

--- lib/attribute-filters.rb
+++ lib/attribute-filters.rb
@@ -10,6 +10,7 @@ require 'attribute-filters/attribute_set_attrquery'
 require 'attribute-filters/helpers'
 require 'attribute-filters/dsl_sets'
 require 'attribute-filters/dsl_filters'
+require 'attribute-filters/common_filters'
 
 if defined? ::Rails
   require 'attribute-filters/railtie'
--- lib/attribute-filters/common_filters.rb
+++ lib/attribute-filters/common_filters.rb
@@ -1,4 +1,79 @@
+# encoding: utf-8
+#
+# Author::    Paweł Wilk (mailto:pw@gnu.org)
+# Copyright:: (c) 2012 by Paweł Wilk
+# License::   This program is licensed under the terms of {file:LGPL-LICENSE GNU Lesser General Public License} or {file:COPYING Ruby License}.
+# 
+# This file contains ActiveModel::AttributeFilters::Common module
+# containing ready-to-use, common filtering methods.
 
+# @abstract This namespace is shared with ActveModel.
+module ActiveModel
+  module AttributeFilters
+    # This module contains common, ready-to-use filtering methods.
+    module Common
+      # Strips attributes from leading and trailing spaces.
+      # 
+      # The attrubutes to be stripped are taken from the attribute set called
+      # +should_be_stripped+. It operates directly on attribute's contents.
+      # 
+      # @return [void]
+      def strip_attributes
+        call_attrs_from_set(:should_be_stripped) { |atr| atr.strip! }
+      end
 
-# todo: manually included module with common filters
+      # Downcases attributes.
+      # 
+      # The attrubutes to be downcased are taken from the attribute set
+      # called +should_be_downcased+. This method is safe to be
+      # used with multibyte strings (containing diacritics).
+      # 
+      # @return [void]
+      def downcase_attributes
+        filter_attrs_from_set(:should_be_downcased) do |atr|
+          atr.mb_chars.downcase.to_s
+        end
+      end
 
+      # Upcases attributes.
+      # 
+      # The attrubutes to be upcased are taken from the attribute set
+      # called +should_be_upcased+. This method is safe to be
+      # used with multibyte strings (containing diacritics).
+      # 
+      # @return [void]
+      def upcase_attributes
+        filter_attrs_from_set(:should_be_upcased) do |atr|
+          atr.mb_chars.upcase.to_s
+        end
+      end
+
+      # Capitalize attributes.
+      # 
+      # The attrubutes to be capitalized are taken from the attribute set
+      # called +should_be_capitalized+. This method is safe to be
+      # used with multibyte strings (containing diacritics).
+      # 
+      # @return [void]
+      def capitalize_attributes
+        filter_attrs_from_set(:should_be_capitalized) do |atr|
+          atr.mb_chars.capitalize.to_s
+        end
+      end
+
+      # Fully capitalize attributes (capitalize each word).
+      # 
+      # The attrubutes to be fully capitalized are taken from the attribute set
+      # called +should_be_fully_capitalized+. This method is safe to be
+      # used with multibyte strings (containing diacritics).
+      # 
+      # @return [void]
+      def fully_capitalize_attributes
+        filter_attrs_from_set(:should_be_fully_capitalized) do |atr|
+          atr.mb_chars.split(' ').map { |n| n.capitalize }.join(' ')
+        end
+      end
+
+    end # module Common
+  end # module AttributeFilters
+end # module ActiveModel
