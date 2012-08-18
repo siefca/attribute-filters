317af234c227ce62f5afcf6728263cda85c20d4a
Paweł Wilk <siefca@gnu.org>
2012-08-18 18:49:33 +0200

    Added filter_attributes DSL method for lazy programmers

--- lib/attribute-filters/common_filters.rb
+++ lib/attribute-filters/common_filters.rb
@@ -10,6 +10,32 @@
 # @abstract This namespace is shared with ActveModel.
 module ActiveModel
   module AttributeFilters
+
+    # This method is a callback method that tries to call all
+    # known filtering methods if they are present.
+    # 
+    # Calling order:
+    #   * split_attributes
+    #   * join_attributes
+    #   * squeeze_attributes
+    #   * strip_attributes
+    #   * upcase_attributes
+    #   * downcase_attributes
+    #   * capitalize_attributes
+    #   * fully_capitalize_attributes
+    #   * titleize_attributes
+    def filter_attributes
+      respond_to?(:split_attributes)            and split_attributes
+      respond_to?(:join_attributes)             and join_attributes
+      respond_to?(:squeeze_attributes)          and squeeze_attributes
+      respond_to?(:strip_attributes)            and strip_attributes
+      respond_to?(:upcase_attributes)           and upcase_attributes
+      respond_to?(:downcase_attributes)         and downcase_attributes
+      respond_to?(:capitalize_attributes)       and capitalize_attributes
+      respond_to?(:fully_capitalize_attributes) and fully_capitalize_attributes
+      respond_to?(:titleize_attributes)         and titleize_attributes
+    end
+
     # This module contains common, ready-to-use filtering methods.
     module Common
 
