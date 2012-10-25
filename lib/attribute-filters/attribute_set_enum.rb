ba4c88480b30d590ba9e20eb8bb7663375dfbce5
Paweł Wilk <siefca@gnu.org>
2012-10-25 15:10:32 +0200

    Enumerators improved: sort, select, reject fixed; all other wrapped to return AttributeSet object

--- lib/attribute-filters/attribute_set_enum.rb
+++ lib/attribute-filters/attribute_set_enum.rb
@@ -11,15 +11,15 @@ module ActiveModel
   class AttributeSet < Hash
     module Enumerable
       # @private
-      def select
-        block_given? ? super : AttributeSet::Enumerator.new(self, :select)
+      def select(&block)
+        block_given? or return AttributeSet::Enumerator.new(self, :select)
+        ActiveModel::AttributeSet.new.tap { |r| each_pair { |k, v| r[k] = v if yield(k, v) } }
       end
 
       # Selects attributes that have setters and getters.
       # @param binding [Object] optional object which should have setters and getters (default: the calling context)
       # @return [AttributeSet::Enumerator, AttributeSet] resulting set or an enumerator if block is not given
       def select_accessible(binding = nil)
-        return AttributeSet::Enumerator.new(self, :accessible) unless block_given?
         if binding.nil?
           select { |a| respond_to?(a) && respond_to?("#{a}=") }
         else
@@ -29,27 +29,36 @@ module ActiveModel
 
       # @private
       def reject
-        block_given? ? super : AttributeSet::Enumerator.new(self, :reject)
+        block_given? or return AttributeSet::Enumerator.new(self, :reject)
+        ActiveModel::AttributeSet.new.tap { |r| each_pair { |k, v| r[k] = v unless yield(k, v) } }
       end
 
       # @private
       def collect
-        if block_given?
-          super { |k, v| yield(k) }
-        else
-          AttributeSet::Enumerator.new(self, :map)
-        end
+        block_given? ? super { |k, v| yield(k) } : AttributeSet::Enumerator.new(self, :map)
       end
       alias_method :map, :collect
 
       # @private
       def each
-        if block_given?
-          super { |k, v| yield(k) }
-        else
-          AttributeSet::Enumerator.new(self, :each)
-        end
+        block_given? ? each_pair { |k, v| yield(k, v) } : AttributeSet::Enumerator.new(self, :each)
       end
+
+      # @private
+      def each_pair
+        block_given? ? super : AttributeSet::Enumerator.new(self, :each_pair)
+      end
+
+      # @private
+      def sort
+        ActiveModel::AttributeSet[super]
+      end
+
+      # @private
+      def sort_by
+        block_given? ? ActiveModel::AttributeSet[super] : AttributeSet::Enumerator.new(self, :sort_by)
+      end
+
     end # module Enumerable
 
     # This class adds enumerator for AttributeSet elements.
